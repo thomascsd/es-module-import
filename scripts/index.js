09eefe30f8f087f6193b53b001a6bb049d51d38b
thomascsd <thomascsd808@gmail.com>
2018-12-16 10:48:46 +0800

    import jquery

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-import { addUser, removeUser } from './contact';
+import { addUser, removeUser } from './contact.js';
+import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js';
+const $ = window.jQuery;
 
 $('#add').on('click', addUser);
 $('#remove').on('click', removeUser);
