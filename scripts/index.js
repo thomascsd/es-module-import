5fc2b02aa8e3882abd2300254842cefdbd906c27
thomascsd <thomascsd808@gmail.com>
2018-12-19 07:49:07 +0800

    調整戴入方式

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 import { addUser, removeUser } from './contact.js';
-import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js';
 const $ = window.jQuery;
 
 $('#add').on('click', addUser);
